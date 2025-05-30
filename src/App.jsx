5de42000f0dece84e8298d32871002a7a7dc9012
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-05-30 11:28:09 +0530

    Conditional UI rendering.

--- src/App.jsx
+++ src/App.jsx
@@ -31,33 +31,58 @@ import './App.css'
 // }
 
 //Working of props
-function ListItem(props) {
-  return (
-    <li>{props.animalName}</li>
-  );
-}
+// function ListItem(props) {
+//   return (
+//     <li>{props.animalName}</li>
+//   );
+// }
+
+// function List(props) {
+//   return (
+//     <ul>
+//       {props.animalList.map((animal) => {
+//        return <ListItem key={animal} animalName={animal}/>
+//       })}
+//     </ul>
+//   );
+// }
+
+
+// function App() {
+//   const animals = ["Lion", "Cow", "Snake", "Lizard"];
+
+//   return (
+//     <div>
+//       <h1>Animalss: </h1>
+//       <List animalList={animals}/>
+//     </div>
+//   );
+// }
+
+
+
+
+// Conditional Rendering of UI
 
 function List(props) {
   return (
     <ul>
-      {props.animalList.map((animal) => {
-       return <ListItem key={animal} animalName={animal}/>
+      {props.animals.map((animal) => {
+        return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
       })}
     </ul>
   );
 }
 
-
 function App() {
   const animals = ["Lion", "Cow", "Snake", "Lizard"];
-
+  
   return (
     <div>
-      <h1>Animalss: </h1>
-      <List animalList={animals}/>
+      <h1>Animals: starting with L</h1>
+      <List animals={animals} />
     </div>
   );
 }
-
 
 export default App;
