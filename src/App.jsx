967db340b583026a7d716a5b7b7dac8af402e06a
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-05-30 11:39:49 +0530

    Conditional rendering to handle different cases.

--- src/App.jsx
+++ src/App.jsx
@@ -64,25 +64,58 @@ import './App.css'
 
 // Conditional Rendering of UI
 
+// function List(props) {
+//   return (
+//     <ul>
+//       {props.animals.map((animal) => {
+//         return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
+//       })}
+//     </ul>
+//   );
+// }
+
+// function App() {
+//   const animals = ["Lion", "Cow", "Snake", "Lizard"];
+  
+//   return (
+//     <div>
+//       <h1>Animals: starting with L</h1>
+//       <List animals={animals} />
+//     </div>
+//   );
+// }
+
+
+// Guarding
+
 function List(props) {
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
   return (
     <ul>
       {props.animals.map((animal) => {
-        return animal.startsWith("L") ? <li key={animal}>{animal}</li> : null;
+        return <li key={animal}>{animal}</li>;
       })}
     </ul>
   );
 }
 
 function App() {
-  const animals = ["Lion", "Cow", "Snake", "Lizard"];
-  
+  const animals = [];
+
   return (
     <div>
-      <h1>Animals: starting with L</h1>
+      <h1>Animals: </h1>
       <List animals={animals} />
     </div>
   );
 }
+
 
 export default App;
